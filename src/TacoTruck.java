ef6346f58b8e99037fe4abe6b5c328926ff55a82
Thomas hale <TH146688@davenportschools.org>
2019-08-28 09:27:08 -0500

    Update work

---
+++ src/TacoTruck.java
@@ -0,0 +1,17 @@
+public class TacoTruck {
+
+    public static void main (String[] args) {
+        System.out.println("TruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruck");
+        System.out.println("TruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruck");
+        System.out.println("TruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruck");
+        System.out.println("TruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruck");
+        System.out.println("TruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruck");
+        System.out.println("TruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruck");
+        System.out.println("TruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruckTruck");
+        System.out.println("TruckTACOTACOTACOTACOTACO                        TACOTACOTACOTruckTACOTACOTACOTruckTACOTACOTACOTruck");
+        System.out.println("TruckTACOTACOTACOTACOTACO                                    TACOTACOTACOTruck");
+        System.out.println("TruckTACOTACOTACOTACOTACO                                    TACOTACOTACOTruck");
+        System.out.println("TruckTACOTACOTACOTACOTACO                                    TACOTACOTACOTruck");
+        System.out.println("TruckTACOTACOTACOTACOTACOTACOTACOTACOTACOTACOTACOTACOTACOTACOTACOTACOTACOTruck");
+    }
+}
