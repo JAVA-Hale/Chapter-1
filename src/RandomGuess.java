9f3e90c05ed7dbf61fa84da95bc32468bfc44716
Thomas hale <TH146688@davenportschools.org>
2019-08-29 09:42:58 -0500

    update change

--- src/RandomGuess.java
+++ src/RandomGuess.java
@@ -3,6 +3,6 @@ import javax.swing.*;
 public class RandomGuess {
     public static void main (String[] args) {
 //        System.out.println("Hello World");
-        JOptionPane.showMessageDialog(null,"This Number Is" + (1 + (int)(Math.random()*10)));
+        JOptionPane.showMessageDialog(null,"This Number Is " + (1 + (int)(Math.random()*10)));
     }
 }
